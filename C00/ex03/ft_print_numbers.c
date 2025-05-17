703df656c799bab8c2ac141cf86cea076820c432
grumbledorf <grumbledorf@localhost.localdomain>
2025-05-18 01:52:52 +0400

    update

--- C00/ex03/ft_print_numbers.c
+++ C00/ex03/ft_print_numbers.c
@@ -1,14 +1,18 @@
 #include <unistd.h>
 
-void ft_print_numbers(void){
-    write(1, "0123456789\n", 11);
-}
+// void ft_print_numbers(void){
+//     write(1, "0123456789\n", 11);
+// }
 
 void ft_print_numbers(void){
-    int i = 0;
-    while (i<=9)
-    {
-        write(1, i, 11);
+    char i = '0';
+    while(i <= '9'){
+        write(1, &i, 1);
+        i++;
     }
-    
+    write(1, "\n", 1);     
+}
+
+int main(void){
+    ft_print_numbers();
 }
